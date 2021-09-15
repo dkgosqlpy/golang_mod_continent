fdb73f2c3967113b4f23dbacb7f8de06d8483973
d k s <dhananjayksharma@gmail.com>
2021-09-15 16:19:49 +0530

    addes solar distance

--- solar/solar.go
+++ solar/solar.go
@@ -8,3 +8,8 @@ func Solarnames() []string {
 	continent := []string{"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"}
 	return continent
 }
+
+func SolarDistance() map[int]string {
+	continent := map[int]string{201001: "Mercury", 301002: "Venus", 401003: "Earth", 501004: "Mars", 601005: "Jupiter", 701006: "Saturn", 801007: "Uranus", 901008: "Neptune", 911009: "Pluto"}
+	return continent
+}
